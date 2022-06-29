4df245c68f85131ec964e92885690174bf6055f5
kaviyarasan <kaviyarasan@dorustree.in>
2022-06-29 15:38:03 +0530

    first commit

---
+++ src/component/card.js
@@ -0,0 +1,24 @@
+import { Card, CardImg,CardBody } from 'reactstrap';
+    import * as React from 'react';
+    import "../App.css";
+  
+  const CardComponent = (props) => {
+
+    return (
+        <div className='cardmainDiv'>
+        <Card className='card-box'>
+          <CardBody className=''>
+          <i className={"fa-solid fa-thumbs-up"}  key={props.id} onClick={props.like}></i>
+          <CardImg  src={props.image} alt="Card image cap" className='cardimages'/>
+            <button className='cardButton border-0' data-testid="card-button" onClick={props.button}>{props.buttonName}</button>
+          </CardBody>
+        </Card>
+        <div className='cardtextdiv mt-3'>
+        <p className='cardtext'>{props.name}</p>
+          <p className='prices'>{props.price}</p>
+          </div>
+        </div>
+    );
+  };
+  
+  export default CardComponent;
